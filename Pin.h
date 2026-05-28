d8c2123d71bce511ccee6cf37a41d1c9960a1d29
RGrabolus <ra.grabolus@gmail.com>
2026-05-28 11:11:46 +0200

    Refactor Pin class with mutex for thread safety

--- Pin.h
+++ Pin.h
@@ -54,3 +54,59 @@ public:
 };
 
 #endif
+#ifndef PIN_H
+#define PIN_H
+
+#include <algorithm>
+#include <mutex>
+#include <thread>
+
+class Pin
+{
+private:
+
+	int steps;
+	double voltage_v;
+	double aref_v;
+
+	mutable std::mutex m;
+
+public:
+
+	double getVoltage() const
+	{
+		std::lock_guard<std::mutex> lock(m);
+
+		return voltage_v;
+	}
+
+	void setVoltage(double voltage_v)
+	{
+		std::lock_guard<std::mutex> lock(m);
+
+		this->voltage_v = voltage_v;
+	}
+
+	unsigned int analogRead()
+	{
+		return ( ((std::min(voltage_v, aref_v) / aref_v ) * steps) - 1 );
+	}
+
+	//2-Parameter Konstruktor
+	Pin (int steps, double aref_v)
+	{
+		this->steps = steps;
+		this->aref_v = aref_v;
+		this->voltage_v = 0;
+	}
+
+	//leerer Konstruktor
+	Pin ()
+	{
+		this->steps = 1024;
+		this->aref_v = 5.0;
+		this->voltage_v = 0;
+	}
+};
+
+#endif
